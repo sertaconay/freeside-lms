43fbfa2dbc32e45e844d98633ce8c0746a7c4380
Sertaç Onay <sertac@onay.me>
2017-09-15 17:49:03 +0200

    eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+const OFF = 0,
+      WARN = 1,
+      ERROR = 2;
+
+module.exports = {
+  'extends': 'airbnb',
+  'rules': {
+    'react/jsx-filename-extension': OFF,
+    'max-len': OFF,
+    'no-console': OFF,
+    'spaced-comment': OFF,
+  },
+};
