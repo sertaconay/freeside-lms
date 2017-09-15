b1ff5e553a3b54c21bd8134720a98ac7bb328ceb
Sertaç Onay <sertac@onay.me>
2017-09-15 17:53:46 +0200

    added general constants

---
+++ core/constants.js
@@ -0,0 +1,4 @@
+export const semanticComponentDirectory = '/static/semantic/components';
+export const staticDirectory = '/static';
+export const semanticCSS = '//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css';
+export const getLinkOfStylesheet = fileName => `${semanticComponentDirectory}/${fileName}.min.css`;
