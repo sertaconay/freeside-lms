649f61ac898a8157509bed96640ec9e5fdca706c
Sertaç Onay <sertac@onay.me>
2017-09-25 15:47:13 +0300

    added dashboard page file

---
+++ pages/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class DashboardPage extends Component {
+  showDashboardPage() {
+    console.log(this);
+  }
+
+  render() {
+    return (
+      <h1>Dashboard</h1>
+    );
+  }
+}
