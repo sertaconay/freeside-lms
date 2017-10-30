fd3ec8c957cfa89c602c60e2814fae044f49ab65
Sertaç Onay <sertac@onay.me>
2017-10-30 22:03:13 +0300

    added and upgraded packages

--- pages/_document.js
+++ pages/_document.js
@@ -10,19 +10,26 @@ export default class SiteDocument extends Document {
   }
 
   static getInitialProps({ renderPage }) {
-    const { html, head, errorHtml, chunks } = renderPage();
-    return { html, head, errorHtml, chunks };
+    const {
+      html, head, errorHtml, chunks,
+    } = renderPage();
+    return {
+      html, head, errorHtml, chunks,
+    };
   }
 
   render() {
     return (
       <html lang="tr">
         <Head>
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0" />
           {this.stylesToUse.map(fileName => (
             <link key={fileName} rel="stylesheet" href={getLinkOfStylesheet(fileName)} />
           ))}
         </Head>
         <body>
+          <span className="sa" />
           <Main />
           <NextScript />
         </body>
